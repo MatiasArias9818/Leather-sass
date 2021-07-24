49d25c9446ac5c47cafa45308f7abb88aebf4419
MatiasArias9818 <matiasarias9818@gmail.com>
2021-07-24 05:29:55 -0300

    code.js added

---
+++ code.js
@@ -0,0 +1,13 @@
+let name = prompt;
+
+hi = "Hi, ";
+name = prompt("Hi, What's your name?");
+question = " . How are You?";
+
+result = hi.concat(name, question);
+
+alert(result);
+
+if ((name = prompt)) {
+  alert("Welcome to Wolf leather workshop");
+}
